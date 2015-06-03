c9a405cae6890a2ba909620d48dd157d638e8b78
Divan <kandiah.divan@gmail.com>
2015-06-03 14:13:12 +0200

    test

---
+++ Vue/gabarit.php
@@ -0,0 +1,16 @@
+<?php
+require_once "header.php";
+?>
+
+    <div id="global">
+        <header id="header_gabarit">
+        </header>
+        <div id="contenu">
+            <?= $contenu ?>
+        </div> <!-- #contenu -->
+        <footer id="footer_gabarit">
+        </footer>
+    </div> <!-- #global -->
+<?php
+require_once "footer.php";
+?>
