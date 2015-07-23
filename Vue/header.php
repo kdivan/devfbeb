304dd2752a99778b3bfb5e8fbd8a4f0768cc34a4
Divan <kandiah.divan@gmail.com>
2015-07-23 02:02:56 +0200

    modif taille canvas

--- Vue/header.php
+++ Vue/header.php
@@ -26,8 +26,7 @@ $cssLink = '<link rel="stylesheet" href="https://code.jquery.com/ui/1.11.1/theme
             xfbml      : true,
             version    : 'v2.3'
         });
-        FB.Canvas.setSize({ width: 810, height: 790 });
-    };
+        FB.Canvas.setSize({ width: 640, height: 480 });    };
     (function(d, s, id){
         var js, fjs = d.getElementsByTagName(s)[0];
         if (d.getElementById(id)) {return;}
